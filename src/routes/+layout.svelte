9d41a9f5ccc8cbdefc84be470c14093dadbae8a6
ngurahbas <ngurahbaskara@gmail.com>
2026-05-30 11:30:35 +0800

    adding skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import ThemeToggle from '$lib/components/ThemeToggle.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<header class="flex items-center justify-end p-4">
+	<ThemeToggle />
+</header>
+
 {@render children()}
